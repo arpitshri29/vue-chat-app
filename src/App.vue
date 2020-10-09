32515213f6a8a1ede6d9506d86e688425fc4a85d
Arpit <arpit@interfacem.com>
2020-10-09 10:20:44 +0530

    - Initial commit

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <h5>Hello, I am developing a chat app.</h5>
+  <h5>~Arpit~</h5>
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+  components: {
+
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
